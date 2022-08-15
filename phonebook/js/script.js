b798a33c8755395289b3873ad041e46a138fb539
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-08-15 10:53:45 +0300

    Update script.js

--- phonebook/js/script.js
+++ phonebook/js/script.js
@@ -1,11 +1,38 @@
 'use strict';
 
 {
-  const phonebook = (selector, title) => {
-    const elem = document.createElement(selector);
-
-    return elem;
+  const createContainer = () => {
+    const container = document.createElement('div');
+    container.classList.add('container');
+    return container;
   };
 
-  window.phonebook = phonebook;
+  const createLogo = (title) => {
+    const h1 = document.createElement('h1');
+    h1.textContent = `Телефонный справочник ${title}`;
+    return h1;
+  };
+
+  const createHeader = () => {
+    const header = document.createElement('hearder');
+    header.classList.add('header');
+
+    const headerContainer = createContainer();
+    header.append(headerContainer);
+
+    header.headerContainer = headerContainer;
+
+    return header;
+  };
+
+  const init = (selector, title) => {
+    const app = document.querySelector(selector);
+    const header = createHeader();
+    const logo = createLogo(title);
+    header.append(logo);
+    app.append(header);
+    return app;
+  };
+
+  window.phonebookInit = init;
 }
