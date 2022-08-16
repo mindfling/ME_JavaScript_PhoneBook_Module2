8f7b59a891ca24c5b980e4d31089b9c5d797dd17
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2022-08-16 15:45:31 +0300

    Update script.js

--- phonebook/js/script.js
+++ phonebook/js/script.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const data = [
+  {
+    name: 'Иван',
+    surname: 'Петров',
+    phone: '+79514545454',
+  },
+  {
+    name: 'Игорь',
+    surname: 'Семёнов',
+    phone: '+79999999999',
+  },
+  {
+    name: 'Семён',
+    surname: 'Иванов',
+    phone: '+79800252525',
+  },
+  {
+    name: 'Мария',
+    surname: 'Попова',
+    phone: '+79876543210',
+  },
+  {
+    name: 'Dmitry',
+    surname: 'Vasylivi4',
+    phone: '+79001234567',
+  },
+];
+
+
 {
   const createContainer = () => {
     const container = document.createElement('div');
@@ -7,14 +36,8 @@
     return container;
   };
 
-  const createLogo = (title) => {
-    const h1 = document.createElement('h1');
-    h1.textContent = `Телефонный справочник ${title}`;
-    return h1;
-  };
-
   const createHeader = () => {
-    const header = document.createElement('hearder');
+    const header = document.createElement('header');
     header.classList.add('header');
 
     const headerContainer = createContainer();
@@ -25,13 +48,104 @@
     return header;
   };
 
-  const init = (selector, title) => {
-    const app = document.querySelector(selector);
+  const createLogo = title => {
+    const h1 = document.createElement('h1');
+    h1.classList.add('logo');
+    h1.textContent = `Телефонный справочник. ${title}`;
+
+    return h1;
+  };
+
+  const createMain = () => {
+    const main = document.createElement('main');
+
+    const mainContainer = createContainer();
+    main.append(mainContainer);
+    main.mainContainer = mainContainer;
+
+    return main;
+  };
+
+  const createButtonGroup = params => {
+    const btnWrapper = document.createElement('div');
+    btnWrapper.classList.add('btn-wrapper');
+
+    const btns = params.map(({className, type, text}) => {
+      const button = document.createElement('button');
+      button.type = type;
+      button.textContent = text;
+      button.className = className;
+      // button.classList.add(className);
+      return button;
+    });
+
+    btnWrapper.append(...btns);
+
+    return {
+      btnWrapper,
+      btns,
+    };
+  };
+
+  const createTable = (data) => {
+    const table = document.createElement('table');
+    table.classList.add('table', 'table-striped');
+
+    const thead = document.createElement('thead');
+    thead.insertAdjacentHTML('beforeend', `
+      <tr>
+        <th class="delete">Удалить</th>
+        <th>Имя</th>
+        <th>Фамилия</th>
+        <th>Телефон</th>
+      </tr>
+    `);
+
+    const tbody = document.createElement('tbody');
+
+    // const rows = data.map({name, surname, phone} => {
+    //   const row = document.createElement('tr');
+    //   const cellName = document.createElement('td');
+    //   cellName.textContent = name;
+    //   const cellSurname = document.createElement('td');
+    //   cellSurname.textContent = surname;
+    //   const cellPhone = document.createElement('td');
+    //   cellPhone.textContent = phone;
+    //   row.append(cellName, cellSurname, cellPhone);
+    //   return row;
+    // });
+    // tbody.append(...rows);
+
+
+    table.append(thead, tbody);
+
+    return table;
+  };
+
+  
+
+  const init = (selectorApp, title) => {
+    const app = document.querySelector(selectorApp);
     const header = createHeader();
     const logo = createLogo(title);
-    header.append(logo);
-    app.append(header);
-    return app;
+    const main = createMain();
+    const buttonGroup = createButtonGroup([
+      {
+        className: 'btn btn-primary mr-3',
+        type: 'button',
+        text: 'Добавить',
+      },
+      {
+        className: 'btn btn-danger',
+        type: 'button',
+        text: 'Удалить',
+      },
+    ]);
+    const table = createTable();
+
+    header.headerContainer.append(logo);
+    main.mainContainer.append(buttonGroup.btnWrapper, table);
+    app.append(header, main);
   };
 
   window.phonebookInit = init;
